54fa66315f4c43bf7d7fcbb02454d70dc5695200
Darren Rambaud <xyzst@users.noreply.github.com>
2019-04-23 20:11:16 -0500

    Learning basic DOM manipulation with querySelector

--- app.js
+++ app.js
@@ -4,7 +4,28 @@ GAME RULES:
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+// Keep track of player scores
+var scores = [0, 0];
+// Keep track of round score
+var roundScore = 0;
+// Who's turn is it? (0 == player1, 1 == player2)
+var activePlayer = 1;
+// Keep track of dice value
+var dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
+
+// Mutate element on HTML document
+document.querySelector("#current-" + activePlayer).textContent = dice;
+// document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>";
+
+// Read element on HTML document
+var x = document.querySelector("#score-0").textContent;
+console.log(x);
+
+// Change CSS style of element
+document.querySelector(".dice").style.display = "none";
